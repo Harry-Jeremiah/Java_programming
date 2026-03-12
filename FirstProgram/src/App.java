b864f16573c515a085b0d79e9bf1e62954cb9630
Harry-Jeremiah <katumbah@sheridancollege.ca>
2026-03-11 22:26:25 -0400

    First Java file

--- FirstProgram/src/App.java
+++ FirstProgram/src/App.java
@@ -1,5 +1,13 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        System.out.println("Hello Fred!");
+
+        System.out.println("Bye, Fred");
+
+        System.out.print("\tUsing tab");
+
+        System.out.print("\nUsing Newline");
     }
 }
